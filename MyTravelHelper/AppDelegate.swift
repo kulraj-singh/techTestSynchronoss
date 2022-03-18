af697a37af1713feb158424f1e92205a12fa2d41
kulraj <kulraj@doctoranywhere.com>
2022-03-18 11:29:49 +0530

    search train screen showing now

--- MyTravelHelper/AppDelegate.swift
+++ MyTravelHelper/AppDelegate.swift
@@ -14,7 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let notice = SearchTrainRouter.createModule()
-        return false
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = notice
+        window?.makeKeyAndVisible()
+        return true
     }
 }
 
